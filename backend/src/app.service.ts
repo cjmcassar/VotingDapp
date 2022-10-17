42f8f902f16907eefd997404349310a7a707efdd
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-16 23:50:53 -0400

    added abi for ballot and token

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,9 +4,11 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import * as TokenizedBallotJson from './assets/TokenizedBallot.json'
+import * as ERC20VotesJson from './assets/ERC20Votes.json'
 
 // Cri's ballot contract from week 3
-const CONTRACT_ADDRESS = '0x8A6A405041FFE6C09DE84cB74cEE5b5767D2C2BE'
+const BALLOT_CONTRACT_ADDRESS = '0x8A6A405041FFE6C09DE84cB74cEE5b5767D2C2BE'
+const TOKEN_CONTRACT_ADDRESS = ''
 
 export interface CastVoteDTO {
   proposal: string
@@ -28,7 +30,8 @@ export class AppService {
   // TODO: Provide types for wallet and signer. Attempted implementation below.
   wallet: ethers.Wallet
   signer: ethers.Signer
-  contract: ethers.Contract
+  tokenContract: ethers.Contract
+  ballotContract: ethers.Contract
 
   constructor() {
     this.provider = ethers.getDefaultProvider('goerli', options)
@@ -36,8 +39,14 @@ export class AppService {
     this.wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
     this.signer = this.wallet.connect(this.provider)
 
-    this.contract = new ethers.Contract(
-      CONTRACT_ADDRESS,
+    this.tokenContract = new ethers.Contract(
+      TOKEN_CONTRACT_ADDRESS,
+      ERC20VotesJson.abi,
+      this.provider,
+    )
+
+    this.ballotContract = new ethers.Contract(
+      BALLOT_CONTRACT_ADDRESS,
       TokenizedBallotJson.abi,
       this.provider,
     )
@@ -45,25 +54,28 @@ export class AppService {
 
   async mint(to: string, amount: number) {
     console.log(`Minting ${amount} tokens to ${to}`)
-    const mintTx = await this.contract.mint(to, amount)
+    const mintTx = await this.tokenContract.mint(to, amount)
     await mintTx.wait()
-    const balance = await this.contract.balanceOf(to)
+    const balance = await this.tokenContract.balanceOf(to)
     console.log(`Minted ${amount} tokens to ${to}. Balance is now ${balance}`)
   }
 
   async castVote(body: CastVoteDTO) {
     // TODO: Pass in signer to contract/transaction. Currently errors saying: 'Error: sending a transaction requires a signer'
     console.log('casting vote')
-    const castVoteTx = await this.contract.vote(body.proposal, body.amount)
+    const castVoteTx = await this.ballotContract.vote(
+      body.proposal,
+      body.amount,
+    )
     const castVoteTxReceipt = await castVoteTx.wait()
     console.log({ castVoteTxReceipt })
   }
 
   async delegate(body: DelegateDTO) {
     // TODO: Find fix for delegate. TokenizedBallot.json abi doesn't have a delegate method.
-    const delegateTx = await this.contract.delegate(body.address)
+    const delegateTx = await this.tokenContract.delegate(body.address)
     await delegateTx.wait()
-    const votesAfterDelegate = await this.contract.getVotes(body.address)
+    const votesAfterDelegate = await this.tokenContract.getVotes(body.address)
     console.log(
       `After self-delegating ${
         body.address
@@ -75,7 +87,7 @@ export class AppService {
 
   async queryWinner() {
     console.log('Query winning proposal')
-    const winningProposal = await this.contract.winnerName()
+    const winningProposal = await this.ballotContract.winnerName()
     const name = ethers.utils.parseBytes32String(winningProposal)
     return name
   }
